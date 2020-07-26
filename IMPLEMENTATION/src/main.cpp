61539432917b0915b430ddabbbe072983850d060
MCLEANS <jackagwaya@gmail.com>
2020-07-26 23:18:47 +0300

    Added UART transmission implementation

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -10,10 +10,19 @@
 
 
 #include "stm32f10x.h"
+#include "USART.h"
+
+custom_libraries::USART	Serial(USART1,GPIOA,10,9,9600);
 			
 
 int main(void)
 {
+	Serial.initialize();
+
+	char junk[] = "Hello World";
+	Serial.println(junk);
+	Serial.println(junk);
+	Serial.println(junk);
 
 	while(1){
 
