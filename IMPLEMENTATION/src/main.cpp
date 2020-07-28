12c18946817d5cdec81208d8dec223796829faf2
MCLEANS <jackagwaya@gmail.com>
2020-07-28 18:06:48 +0300

    added UART recieve implementation

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -13,18 +13,28 @@
 #include "USART.h"
 
 custom_libraries::USART	Serial(USART1,GPIOA,10,9,9600);
-			
+
+extern "C" void USART1_IRQHandler(){
+	//Check if we are here because USART1 received data
+	if(USART1->SR & USART_SR_RXNE){
+		//Disable the flag
+		USART1->SR &= ~USART_SR_RXNE;
+		//read USART->DR into receive buffer
+		Serial.read_string();
+
+	}
+}
 
 int main(void)
 {
 	Serial.initialize();
 
-	char junk[] = "Hello World";
-	Serial.println(junk);
-	Serial.println(junk);
-	Serial.println(junk);
+	//Setup UART NVIC
+	NVIC_SetPriority(USART1_IRQn,0x03);
+	NVIC_EnableIRQ(USART1_IRQn);
 
 	while(1){
-
+		Serial.println(Serial.receive_buffer);
+		for(int i=0;i<100000;i++){}
 	}
 }
